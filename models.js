ad7fefde2ed4fb5e8f1b8fd5f074658a1eb3f27a
Ashley Williams <ashley.williams@strongmind.com>
2025-01-19 17:18:43 -0700

    updated models.js for new lowercase scheme

--- models.js
+++ models.js
@@ -1,37 +1,32 @@
-// Require Mongoose package
 const mongoose = require('mongoose');
 
-// Define the schema for the Movies collection
 const movieSchema = mongoose.Schema({
-    Title: { type: String, required: true },
-    Description: { type: String, required: true },
-    Genre: {
-        Name: String,
-        Description: String
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    genre: {
+        name: String,
+        description: String
     },
-    Director: {
-        Name: String,
-        Bio: String,
-        Birth: Date
+    director: {
+        name: String,
+        bio: String,
+        birth: Date
     },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+    actors: [String],
+    imagepath: String,
+    featured: Boolean
 });
 
-// Define the schema for the Users collection
 const userSchema = mongoose.Schema({
-    Username: { type: String, required: true },
-    Password: { type: String, required: true },
-    Email: { type: String, required: true },
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    birthday: Date,
+    favoritemovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-// Create the models from the schemas
 const Movie = mongoose.model('Movie', movieSchema);
 const User = mongoose.model('User', userSchema);
 
-// Export the models for use in other files
 module.exports.Movie = Movie;
 module.exports.User = User;
